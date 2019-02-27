11f3e2dffc32092af95b9232578eddfba9c00b3d
Xing <s1879281@hotmail.com>
2019-02-27 16:34:28 +0000

    qlearning 5000

--- Exercise2/QLearningAgent.sh
+++ Exercise2/QLearningAgent.sh
@@ -4,9 +4,9 @@
 # Cannot run an environment where defending agents exist but none are playing
 # goalkeeper
 
-./../../../bin/HFO --defense-agents=2 --offense-agents=1 --offense-on-ball 11 --trials 500 --deterministic --discrete=True --frames-per-trial 2000 --untouched-time 2000 &
+./../../../bin/HFO --defense-agents=2 --offense-agents=1 --offense-on-ball 11 --trials 5000 --deterministic --discrete=True --frames-per-trial 2000 --untouched-time 2000 &
 sleep 5
-./DiscreteHFO/Initiator.py --numTrials=500 --numPlayingDefenseNPCs=1 --numAgents=1 &
+./DiscreteHFO/Initiator.py --numTrials=5000 --numPlayingDefenseNPCs=1 --numAgents=1 &
 echo "Environment Initialized"
 # Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
 
