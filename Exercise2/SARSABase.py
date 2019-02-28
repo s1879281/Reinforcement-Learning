0bc5edeb947eb6fb0aa62870ff766415cd8d048f
Xing <s1879281@hotmail.com>
2019-02-28 00:25:10 +0000

    sarsa

--- Exercise2/SARSABase.py
+++ Exercise2/SARSABase.py
@@ -1,91 +1,124 @@
 #!/usr/bin/env python3
 # encoding utf-8
 
-from DiscreteHFO.HFOAttackingPlayer import HFOAttackingPlayer
-from DiscreteHFO.Agent import Agent
 import argparse
+import random
+
+from DiscreteHFO.Agent import Agent
+from DiscreteHFO.HFOAttackingPlayer import HFOAttackingPlayer
+
 
 class SARSAAgent(Agent):
-	def __init__(self, learningRate, discountFactor, epsilon, initVals=0.0):
-		super(SARSAAgent, self).__init__()
+    def __init__(self, learningRate, discountFactor, epsilon, initVals=0.0):
+        super(SARSAAgent, self).__init__()
+        self.initLearningRate = learningRate
+        self.learningRate = learningRate
+        self.discountFactor = discountFactor
+        self.initEpsilon = epsilon
+        self.epsilon = epsilon
+        self.initVals = initVals
+        self.QValueTable = {}
+        self.stateList = []
+        self.actionList = []
+        self.rewardList = []
+        self.statusList = []
 
-	def learn(self):
-		raise NotImplementedError
+    def learn(self):
+        update = self.learningRate * (
+                self.rewardList[-2] + self.discountFactor * self.QValueTable[
+            (self.stateList[-1], self.actionList[-1])] - self.QValueTable[(self.stateList[-2], self.actionList[-2])])
+        self.QValueTable[(self.stateList[-2], self.actionList[-2])] += update
 
-	def act(self):
-		raise NotImplementedError
+        return update
 
-	def setState(self, state):
-		raise NotImplementedError
+    def act(self):
+        randomNum = random.random()
+        if randomNum > self.epsilon:
+            return random.choice(self.possibleActions)
+        else:
+            actionDict = {key[1]: value for key, value in self.QValueTable.items() if key[0] == self.curState}
+            return random.choice([action for action, value in actionDict.items() if value == max(actionDict.values())])
 
-	def setExperience(self, state, action, reward, status, nextState):
-		raise NotImplementedError
+    def setState(self, state):
+        self.curState = state
+        if not (self.curState, 'KICK') in self.QValueTable.keys():
+            self.QValueTable.update({(self.curState, action): self.initVals for action in self.possibleActions})
 
-	def computeHyperparameters(self, numTakenActions, episodeNumber):
-		raise NotImplementedError
+    def setExperience(self, state, action, reward, status, nextState):
+        self.stateList.append(state)
+        self.actionList.append(action)
+        self.rewardList.append(reward)
+        self.statusList.append(status)
+        if not action:
+            self.QValueTable[(state, action)] = 0
 
-	def toStateRepresentation(self, state):
-		raise NotImplementedError
+    def computeHyperparameters(self, numTakenActions, episodeNumber):
+        learningRate = self.initLearningRate * 0.95 ** (episodeNumber // 500)
+        epsilon = self.initEpsilon * 0.98 ** (episodeNumber // 500)
 
-	def reset(self):
-		raise NotImplementedError
+        return learningRate, epsilon
 
-	def setLearningRate(self, learningRate):
-		raise NotImplementedError
+    def toStateRepresentation(self, state):
+        return str(state)
 
-	def setEpsilon(self, epsilon):
-		raise NotImplementedError
+    def reset(self):
+        pass
+
+    def setLearningRate(self, learningRate):
+        self.learningRate = learningRate
+
+    def setEpsilon(self, epsilon):
+        self.epsilon = epsilon
+
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--id', type=int, default=0)
-	parser.add_argument('--numOpponents', type=int, default=0)
-	parser.add_argument('--numTeammates', type=int, default=0)
-	parser.add_argument('--numEpisodes', type=int, default=500)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', type=int, default=0)
+    parser.add_argument('--numOpponents', type=int, default=0)
+    parser.add_argument('--numTeammates', type=int, default=0)
+    parser.add_argument('--numEpisodes', type=int, default=500)
 
-	args=parser.parse_args()
-	
-	numEpisodes = args.numEpisodes
-	# Initialize connection to the HFO environment using HFOAttackingPlayer
-	hfoEnv = HFOAttackingPlayer(numOpponents = args.numOpponents, numTeammates = args.numTeammates, agentId = args.id)
-	hfoEnv.connectToServer()
+    args = parser.parse_args()
 
-	# Initialize a SARSA Agent
-	agent = SARSAAgent(0.1, 0.99)
+    numEpisodes = args.numEpisodes
+    # Initialize connection to the HFO environment using HFOAttackingPlayer
+    hfoEnv = HFOAttackingPlayer(numOpponents=args.numOpponents, numTeammates=args.numTeammates, agentId=args.id)
+    hfoEnv.connectToServer()
 
-	# Run training using SARSA
-	numTakenActions = 0 
-	for episode in range(numEpisodes):	
-		agent.reset()
-		status = 0
+    # Initialize a SARSA Agent
+    agent = SARSAAgent(0.1, 0.99, 1.0)
 
-		observation = hfoEnv.reset()
-		nextObservation = None
-		epsStart = True
+    # Run training using SARSA
+    numTakenActions = 0
+    for episode in range(numEpisodes):
+        agent.reset()
+        status = 0
 
-		while status==0:
-			learningRate, epsilon = agent.computeHyperparameters(numTakenActions, episode)
-			agent.setEpsilon(epsilon)
-			agent.setLearningRate(learningRate)
-			
-			obsCopy = observation.copy()
-			agent.setState(agent.toStateRepresentation(obsCopy))
-			action = agent.act()
-			numTakenActions += 1
+        observation = hfoEnv.reset()
+        nextObservation = None
+        epsStart = True
 
-			nextObservation, reward, done, status = hfoEnv.step(action)
-			print(obsCopy, action, reward, nextObservation)
-			agent.setExperience(agent.toStateRepresentation(obsCopy), action, reward, status, agent.toStateRepresentation(nextObservation))
-			
-			if not epsStart :
-				agent.learn()
-			else:
-				epsStart = False
-			
-			observation = nextObservation
+        while status == 0:
+            learningRate, epsilon = agent.computeHyperparameters(numTakenActions, episode)
+            agent.setEpsilon(epsilon)
+            agent.setLearningRate(learningRate)
 
-		agent.setExperience(agent.toStateRepresentation(nextObservation), None, None, None, None)
-		agent.learn()
+            obsCopy = observation.copy()
+            agent.setState(agent.toStateRepresentation(obsCopy))
+            action = agent.act()
+            numTakenActions += 1
 
-	
+            nextObservation, reward, done, status = hfoEnv.step(action)
+            agent.setExperience(agent.toStateRepresentation(obsCopy), action, reward, status,
+                                agent.toStateRepresentation(nextObservation))
+
+            if not epsStart:
+                agent.learn()
+            else:
+                epsStart = False
+
+            observation = nextObservation
+
+        agent.setExperience(agent.toStateRepresentation(nextObservation), None, None, None, None)
+        agent.learn()
--- Exercise2/unit_test.py
+++ Exercise2/unit_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from QLearningBase import QLearningAgent
-
+from SARSABase import SARSAAgent
 
 class RLTest(unittest.TestCase):
     def test_Qlearning(self):
@@ -19,6 +19,32 @@ class RLTest(unittest.TestCase):
                                 agent.toStateRepresentation(nextObservation))
             update = agent.learn()
             self.assertAlmostEqual(update, trueUpdate, places=7)
+
+    def test_SARSA(self):
+        agent = SARSAAgent(learningRate=0.1, discountFactor=1, epsilon=1.0)
+        agent.setEpsilon(1.0)
+        agent.setLearningRate(0.1)
+
+        epsStart = True
+        status = 0
+
+        for obsCopy, action, reward, nextObservation, trueUpdate in zip(
+                [((1, 1), (2, 1)), ((2, 1), (2, 1)), ((1, 1), (2, 1))], ['DRIBBLE_RIGHT', 'DRIBBLE_LEFT', 'KICK'],
+                [-0.4, 0.0, 1.0], [((2, 1), (2, 1)), ((1, 1), (2, 1)), 'GOAL'], [None, -0.04, 0.0]):
+            agent.setState(agent.toStateRepresentation(obsCopy))
+
+            agent.setExperience(agent.toStateRepresentation(obsCopy), action, reward, status,
+                                agent.toStateRepresentation(nextObservation))
+            if not epsStart:
+                update = agent.learn()
+                self.assertAlmostEqual(update, trueUpdate, places=7)
+            else:
+                epsStart = False
+                self.assertAlmostEqual(None, trueUpdate, places=7)
+
+        agent.setExperience('GOAL', None, None, None, None)
+        update = agent.learn()
+        self.assertAlmostEqual(update, 0.1, places=7)
 
 
 if __name__ == '__main__':
