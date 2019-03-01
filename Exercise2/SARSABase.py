9dac27d3558c41db5b91efda1e40dd01b9166954
Xing <s1879281@hotmail.com>
2019-03-01 04:34:47 +0000

    exercise2 modified

--- Exercise2/SARSABase.py
+++ Exercise2/SARSABase.py
@@ -55,7 +55,7 @@ class SARSAAgent(Agent):
 
     def computeHyperparameters(self, numTakenActions, episodeNumber):
         learningRate = self.initLearningRate * 0.9 ** (episodeNumber // 100)
-        epsilon = self.initEpsilon * ((1 - 1 / (1 + np.exp(-numTakenActions / 250))) * 2 * 0.9 + 0.1)
+        epsilon = self.initEpsilon * ((1 - 1 / (1 + np.exp(-numTakenActions / 250))) * 2 * 0.95 + 0.05)
 
         return learningRate, epsilon
 
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     hfoEnv.connectToServer()
 
     # Initialize a SARSA Agent
-    agent = SARSAAgent(0.5, 0.99, 1.0)
+    agent = SARSAAgent(0.5, 0.95, 1.0)
 
     # Run training using SARSA
     numTakenActions = 0
