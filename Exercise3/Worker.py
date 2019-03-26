9d3029d7f39c971068da8f6736fc0e0548856732
s1879281 <s1879281@sms.ed.ac.uk>
2019-03-26 23:24:18 +0000

    worker

--- Exercise3/Worker.py
+++ Exercise3/Worker.py
@@ -29,7 +29,6 @@ def train(idx, args, value_network, target_value_network, optimizer, lock, count
 
             pred_value = computePrediction(curState, action, value_network)
             target_value = computeTargets(reward, nextState, args.discountFactor, done, target_value_network)
-            print('target_value',target_value)
             loss = criterion(pred_value, target_value)
             loss.backward()
 
